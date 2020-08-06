2e5703e6ad0316f2876eaa401c21f0ab1e08c0d3
CodeMhasan <mahmudul.pondit@gmail.com>
2020-08-06 20:43:58 +0600

    update

--- scraper.js
+++ scraper.js
@@ -14,26 +14,31 @@ const puppeteer = require('puppeteer');
 (async () => {
   const browser = await puppeteer.launch({headless: false});
   const page = await browser.newPage();
-  await page.waitFor(2000);
-  await page.goto('https://bikroy.com/en/users/login?action=login&redirect-url=/');
-  await page.waitFor(2000);
-  await page.type("#input_email", "jemafad894@ofmailer.net");
-  await page.waitFor(2000);
-  await page.type("#input_password", "@jQNqDz-s?#7Fu9");
-  await page.waitFor(2000);
-  await page.click('.gtm-email-login');
-  await page.waitFor(2000);
-  await page.waitForSelector('.hide-for-inactive', {visible: true});
 
-  await page.screenshot({path: 'schrenshots/$.png'});
+  await page.waitFor(2000);
+  await page.goto('https://www.startech.com.bd/account/login');
+
+  await page.waitFor(2000);
+  await page.type("#input-email", "mhasan.meet@gmail.com");
+
+  await page.waitFor(2000);
+  await page.type("#input-password", "Frj5nHBFH!ZJmqP");
+
+  await page.waitFor(2000);
+  await page.click('.btn');
+
+  await page.waitFor(2000);
+  await page.waitForSelector('.hidden-xs', {visible: true});
+
+  await page.screenshot({path: 'schrenshots/pic.png'});
 
   await browser.close();
 })();
 
-//jemafad894@ofmailer.net
+//mhasan.meet@gmail.com
 //Goto thia page
 //https://bikroy.com/en/users/login?action=login&redirect-url=/
 //input email #input_email
-//input password #input_password
+//input password #Frj5nHBFH!ZJmqP
 //click login .gtn_email_login
 //take screenshot
